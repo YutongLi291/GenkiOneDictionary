5a4861bb089bfb9a7e9b82e924347b30b102f610
rainbowzom <rainbowzom@gmail.com>
2020-04-28 23:04:00 -0700

    some stuff

--- app/src/main/java/com/yrlapps/genkivocab/WordActivity.java
+++ app/src/main/java/com/yrlapps/genkivocab/WordActivity.java
@@ -11,11 +11,19 @@ class WordActivity extends AppCompatActivity {
     private TextView kanjiTextView;
     private TextView romajiTextView;
     private TextView englishTextView;
+    private TextView wordTypeTextView;
     private int position;
 
     @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word);
+        kanaTextView = findViewById(R.id.kana_view);
+        kanjiTextView = findViewById(R.id.kanji_view);
+        romajiTextView = findViewById(R.id.romaji_view);
+        englishTextView = findViewById(R.id.english_view);
+        wordTypeTextView = findViewById(R.id.wordtype_view);
+        
     }
 
 }
